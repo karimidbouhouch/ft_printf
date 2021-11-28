75916cd0fe38269bf9af32b197c4e2d10b56fd1b
karimidbouhouch <karim.idbouhouch@gmail.com>
2021-11-28 17:40:43 +0100

    edit ft_printf function

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: kid-bouh <kid-bouh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 11:23:32 by kid-bouh          #+#    #+#             */
-/*   Updated: 2021/11/23 22:37:19 by kid-bouh         ###   ########.fr       */
+/*   Updated: 2021/11/24 12:47:18 by kid-bouh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,47 +18,47 @@ static int	ft_percent(void)
 	return (1);
 }
 
-static void	ft_printf_2(char str, int *count, void *args)
+static void	ft_printf_2(char c, int *count, void *args)
 {
-	if (str == 's')
+	if (c == 's')
 		ft_putstr((char *)args, count);
-	else if (str == 'd' || str == 'i')
+	else if (c == 'd' || c == 'i')
 		ft_putnbr((int)args, count);
-	else if (str == 'X')
+	else if (c == 'X')
 		ft_puthex_upper((unsigned int)args, count);
-	else if (str == 'x')
+	else if (c == 'x')
 		ft_puthex_lower((unsigned int)args, count);
-	else if (str == 'u')
+	else if (c == 'u')
 		ft_putdec((unsigned int)args, count);
-	else if (str == 'p')
+	else if (c == 'p')
 		ft_putadr((unsigned long)args, count);
 }
 
-int	ft_printf(const char *str, ...)
+int	ft_printf(const char *s, ...)
 {
 	int		i;
 	int		count;
-	va_list	format;
+	va_list	arg_list;
 
 	i = 0;
 	count = 0;
-	va_start(format, str);
-	while (str[i])
+	va_start(arg_list, s);
+	while (s[i])
 	{
-		if (str[i] != '%')
-			count += ft_putchar(str[i]);
-		else if (str[i] == '%')
+		if (s[i] != '%')
+			count += ft_putchar(s[i]);
+		else if (s[i] == '%')
 		{
 			i++;
-			if (str[i] == 'c')
-				count += ft_putchar(va_arg(format, int));
-			else if (str[i] == '%')
+			if (s[i] == 'c')
+				count += ft_putchar(va_arg(arg_list, int));
+			else if (s[i] == '%')
 				count += ft_percent();
 			else
-				ft_printf_2(str[i], &count, va_arg(format, void *));
+				ft_printf_2(s[i], &count, va_arg(arg_list, void *));
 		}
 		i++;
 	}
-	va_end(format);
+	va_end(arg_list);
 	return (count);
 }
